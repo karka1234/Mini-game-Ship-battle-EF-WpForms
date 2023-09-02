19d5badc1a5b1349a0a345dfed22921f0f0fdab9
Karolis_PC <karsi@SMEKLA>
2023-09-02 17:28:39 +0300

    Add project files.

---
+++ Ship Battle BASIC/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace Ship_Battle_BASIC
+{
+    public partial class Form1 : Form
+    {
+        private Button reportButton = new Button();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+            FillEnemyCell();
+        }
+        private void Init()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var col = new DataGridViewButtonColumn();
+                col.Name = i.ToString();
+                col.Width = 40;
+                dataGridView1.Columns.Add(col);
+
+                var row = new DataGridViewRow();
+                row.HeaderCell.Value = Convert.ToChar(65 + i).ToString();
+                row.Height = 40;
+                dataGridView1.Rows.Add(row);
+            }
+
+            dataGridView1.AllowUserToAddRows = false; 
+
+        }
+        private void FillEnemyCell()
+        {
+            Random random = new Random();   
+            for(int i = 0;i < dataGridView1.Rows.Count; i++) 
+            {
+                for(int j = 0; j < dataGridView1.Columns.Count; j++) 
+                {
+                    dataGridView1[i, j].Value = random.Next(0,2);
+
+                }
+            }   
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var aaa = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.Equals(1))
+            {
+                //MessageBox.Show("HIT");
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = 10;
+            }
+        }
+    }
+}
